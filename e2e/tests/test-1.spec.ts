75a9d7ba84f772f69ec9d7a14d956f1231da0757
Lagniau Kenneth <lagniaukenneth@gmail.com>
2025-09-27 13:26:08 +0200

    pipeline changes

--- e2e/tests/test-1.spec.ts
+++ e2e/tests/test-1.spec.ts
@@ -16,12 +16,11 @@ test.describe("Assesment", () => {
   test.beforeAll(async ({ browser }) => {
     const context = await browser.newContext();
     page = await context.newPage();
-    context.cookies();
-    // bolPage = new BolPage(page);
-    // filterSortPage = new FilterSortPage(page);
-    // pdpPage = new PdpPage(page);
-    // interceptor = new InterceptorPage(page);
-    // productlistPage = new ProductListPage(page);
+    bolPage = new BolPage(page);
+    filterSortPage = new FilterSortPage(page);
+    pdpPage = new PdpPage(page);
+    interceptor = new InterceptorPage(page);
+    productlistPage = new ProductListPage(page);
     await page.goto("https://www.bol.com");
   });
 
@@ -30,17 +29,14 @@ test.describe("Assesment", () => {
   });
 
   test("Navigate to the homepage of bol.com", async () => {
-    await expect(page).toHaveURL("https://www.bol.com");
+    await expect(page).toHaveURL("https://www.bol.com/nl/nl/");
   });
 
-  // test("Accept cookie banner", async ({page}) => {
-  //   await page.goto("https://www.bol.com");
-  //   bolPage = new BolPage(page);
-  //   await bolPage.acceptCookies();
-  // });
+  test("Accept cookie banner", async () => {
+    await bolPage.acceptCookies();
+  });
 
-  test('Verify search input is visible and search for "lego"', async ({page}) => {
-    bolPage = new BolPage(page);
+  test('Verify search input is visible and search for "lego"', async () => {
     await bolPage.expectSearchInputVisible();
     await bolPage.searchProduct("lego");
     await bolPage.clickSearchButton();
@@ -48,122 +44,122 @@ test.describe("Assesment", () => {
     await page.screenshot({ path: "resultpage.png" });
   });
 
-//   test('Click on the "Speelgoed" filter', async () => {
-//     await filterSortPage.clickFilter();
-//   });
+  test('Click on the "Speelgoed" filter', async () => {
+    await filterSortPage.clickFilter();
+  });
 
-//   test('Verify breadcrumb shows "lego in Speelgoed"', async () => {
-//     await filterSortPage.breadcrumbVisible();
-//   });
+  test('Verify breadcrumb shows "lego in Speelgoed"', async () => {
+    await filterSortPage.breadcrumbVisible();
+  });
 
-//   test("Extract all product prices and verify they are sorted from low to high", async () => {
-//     await filterSortPage.sortDropdownMenu();
-//     await filterSortPage.sortByPriceLowToHigh();
-//     await page.waitForTimeout(3000);
+  test("Extract all product prices and verify they are sorted from low to high", async () => {
+    await filterSortPage.sortDropdownMenu();
+    await filterSortPage.sortByPriceLowToHigh();
+    await page.waitForTimeout(3000);
 
-//     const prices = await filterSortPage.getAllProductPrices();
-//     const sortedPricesAsc = [...prices].sort((a, b) => a - b);
+    const prices = await filterSortPage.getAllProductPrices();
+    const sortedPricesAsc = [...prices].sort((a, b) => a - b);
 
-//     expect(prices).toEqual(sortedPricesAsc);
-//     expect(prices.length).toBeGreaterThanOrEqual(3);
-//     await page.screenshot({ path: "sortedpage.png" });
-//   });
+    expect(prices).toEqual(sortedPricesAsc);
+    expect(prices.length).toBeGreaterThanOrEqual(3);
+    await page.screenshot({ path: "sortedpage.png" });
+  });
 
-//   test('Verify result is with title"', async () => {
-//     const firstTitle = await bolPage.getProductTitleByName("LEGO").first();
-//     await expect(firstTitle).toBeVisible();
-//   });
+  test('Verify result is with title"', async () => {
+    const firstTitle = await bolPage.getProductTitleByName("LEGO").first();
+    await expect(firstTitle).toBeVisible();
+  });
 
-//   test("Verify prices are visible and formatted correctly", async () => {
-//     const prices = await filterSortPage.getAllProductPrices();
-//     expect(prices.length).toBeGreaterThanOrEqual(1);
-//     const getAllPriceSpans = filterSortPage.getAllPriceSpans();
-//   });
+  test("Verify prices are visible and formatted correctly", async () => {
+    const prices = await filterSortPage.getAllProductPrices();
+    expect(prices.length).toBeGreaterThanOrEqual(1);
+    const getAllPriceSpans = filterSortPage.getAllPriceSpans();
+  });
 
-//   test("Click on the first product, opens in new tab", async () => {
-//     await pdpPage.clickFirstProduct();
-//   });
+  test("Click on the first product, opens in new tab", async () => {
+    await pdpPage.clickFirstProduct();
+  });
 
-//   test("Check title is visible", async () => {
-//     expect(await pdpPage.firstProduct).not.toBeNull();
-//     await pdpPage.isTitleVisible();
-//   });
+  test("Check title is visible", async () => {
+    expect(await pdpPage.firstProduct).not.toBeNull();
+    await pdpPage.isTitleVisible();
+  });
 
-//   test("Check price is visible", async () => {
-//     expect(await pdpPage.firstProduct).not.toBeNull();
-//     await pdpPage.isPriceVisible();
-//   });
+  test("Check price is visible", async () => {
+    expect(await pdpPage.firstProduct).not.toBeNull();
+    await pdpPage.isPriceVisible();
+  });
 
-//   test("Check product availability is visible", async () => {
-//     expect(await pdpPage.firstProduct).not.toBeNull();
-//     await pdpPage.isAvailableVisible();
-//   });
+  test("Check product availability is visible", async () => {
+    expect(await pdpPage.firstProduct).not.toBeNull();
+    await pdpPage.isAvailableVisible();
+  });
 
-//   test('Check "Add to cart" button is visible', async () => {
-//     await pdpPage.isAddToCartButtonVisible();
-//   });
+  test('Check "Add to cart" button is visible', async () => {
+    await pdpPage.isAddToCartButtonVisible();
+  });
 
-//   test("Click Add to Cart but block cart request", async ({ page }) => {
-//     await page.route("**/order/basket/addItems.html**", (route) => {
-//       route.abort();
-//     });
+  test("Click Add to Cart but block cart request", async ({ page }) => {
+    await page.route("**/order/basket/addItems.html**", (route) => {
+      route.abort();
+    });
 
-//     await interceptor.blockAddToCartRequests();
-//     await page.screenshot({ path: "before-add-to-cart.png" });
-//     await pdpPage.clickToCartButton();
-//     await page.screenshot({ path: "after-add-to-cart.png" });
-//   });
+    await interceptor.blockAddToCartRequests();
+    await page.screenshot({ path: "before-add-to-cart.png" });
+    await pdpPage.clickToCartButton();
+    await page.screenshot({ path: "after-add-to-cart.png" });
+  });
 
-//   test.describe("Pagination Test", () => {
-//     test.beforeAll(async ({ browser }) => {
-//       const context = await browser.newContext();
-//       page = await context.newPage();
-//       bolPage = new BolPage(page);
-//       filterSortPage = new FilterSortPage(page);
-//       pdpPage = new PdpPage(page);
-//       interceptor = new InterceptorPage(page);
-//       productlistPage = new ProductListPage(page);
-//       await page.goto("https://www.bol.com");
-//     });
+  test.describe("Pagination Test", () => {
+    test.beforeAll(async ({ browser }) => {
+      const context = await browser.newContext();
+      page = await context.newPage();
+      bolPage = new BolPage(page);
+      filterSortPage = new FilterSortPage(page);
+      pdpPage = new PdpPage(page);
+      interceptor = new InterceptorPage(page);
+      productlistPage = new ProductListPage(page);
+      await page.goto("https://www.bol.com");
+    });
 
-//     test.afterAll(async () => {
-//       await page.close();
-//     });
+    test.afterAll(async () => {
+      await page.close();
+    });
 
-//     test("Navigate to the homepage of bol.com", async () => {
-//       await expect(page).toHaveURL("https://www.bol.com/nl/nl/");
-//     });
+    test("Navigate to the homepage of bol.com", async () => {
+      await expect(page).toHaveURL("https://www.bol.com/nl/nl/");
+    });
 
-//     test("Accept cookie banner", async () => {
-//       await bolPage.acceptCookies();
-//     });
+    test("Accept cookie banner", async () => {
+      await bolPage.acceptCookies();
+    });
 
-//     test('Verify search input is visible and search for "lego"', async () => {
-//       await bolPage.expectSearchInputVisible();
-//       await bolPage.searchProduct("lego");
-//       await bolPage.clickSearchButton();
-//       await expect(page).toHaveURL(/searchtext=lego/i);
-//       await page.screenshot({ path: "resultpage.png" });
-//     });
+    test('Verify search input is visible and search for "lego"', async () => {
+      await bolPage.expectSearchInputVisible();
+      await bolPage.searchProduct("lego");
+      await bolPage.clickSearchButton();
+      await expect(page).toHaveURL(/searchtext=lego/i);
+      await page.screenshot({ path: "resultpage.png" });
+    });
 
-//     test('Click on the "Speelgoed" filter', async () => {
-//       await filterSortPage.clickFilter();
-//     });
+    test('Click on the "Speelgoed" filter', async () => {
+      await filterSortPage.clickFilter();
+    });
 
-//     test("Navigate, check for titles on page 1 & 2 and verify they're not equal", async () => {
-//       const titlesPage1 = await productlistPage.getAllTitles();
-//       const firstFiveTitlesPage1 = titlesPage1.slice(0, 5);
+    test("Navigate, check for titles on page 1 & 2 and verify they're not equal", async () => {
+      const titlesPage1 = await productlistPage.getAllTitles();
+      const firstFiveTitlesPage1 = titlesPage1.slice(0, 5);
 
-//       await page.screenshot({ path: "resultpage1.png" });
-//       await productlistPage.navigateToPage2();
+      await page.screenshot({ path: "resultpage1.png" });
+      await productlistPage.navigateToPage2();
 
-//       await page.waitForTimeout(3000);
-//       await page.screenshot({ path: "resultpage2.png" });
+      await page.waitForTimeout(3000);
+      await page.screenshot({ path: "resultpage2.png" });
 
-//       const titlesPage2 = await productlistPage.getAllTitles();
-//       const firstFiveTitlesPage2 = titlesPage2.slice(0, 5);
+      const titlesPage2 = await productlistPage.getAllTitles();
+      const firstFiveTitlesPage2 = titlesPage2.slice(0, 5);
 
-//       expect(firstFiveTitlesPage1).not.toEqual(firstFiveTitlesPage2);
-//     });
-//   });
+      expect(firstFiveTitlesPage1).not.toEqual(firstFiveTitlesPage2);
+    });
+  });
 });
